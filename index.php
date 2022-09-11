3dbfbd3523f87c3835c301bec0d2b811800d53aa
DESKTOP-87JT63K\lj <jeanpierresung3@gmail.com>
2022-09-11 19:50:58 +0900

    function 만들기

--- index.php
+++ index.php
@@ -1,25 +1,65 @@
+<?php
+    function print_title(){
+        if(isset($_GET['id'])){
+            echo $_GET['id'];          
+        } else {
+            echo 'Welcome';
+        }
+    }
+    
+    function print_description(){
+        if(isset($_GET['id'])) {
+            echo file_get_contents('./data/'.$_GET['id']);
+        } else {
+            echo "Hello PHP 에 대한 내용들입니다.";
+        }
+    }
+
+    function print_list(){
+        $list = scandir('./data');
+        $i = 0;
+        while($i < count($list)){
+            if ($list[$i] != '.'){
+                if ($list[$i] != '..') {
+                    echo "
+                        <li>
+                            <a href \".index.php?id=$list[$i]\"> $list[$i]</a> 
+                        </li> \n
+                    ";
+                }
+            }
+            $i+=1;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+    <?php
+        print_title();
+    ?>
+    </title>
 </head>
 <body>
-    <h1>WEB</h1>
+    <h1>
+        WEB
+    </h1>
     <ol>
-        <li><a href="index.php?id=HTML">HTML</a></li>
-        <li><a href="index.php?id=CSS">CSS</a></li>
-        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+        <?php
+           print_list();
+        ?>
     </ol>
     <h2>
         <?php
-            echo $_GET['id'];
+            print_title();
         ?>
     </h2>
     <?php
-        echo file_get_contents("data/".$_GET['id']);
+        print_description();
     ?>
 </body>
 </html>
